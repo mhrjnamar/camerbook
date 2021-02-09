62e0055614817d908a2e1a8ac9e3cd526e463957
maltortan <maharjan_asiz@hotmail.com>
2021-02-10 01:10:13 +0545

    alret box sign in message

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -18,72 +18,108 @@ class _LoginScreenState extends State<LoginScreen> {
         appBar: AppBar(
           title: Text("Login"),
         ),
-        body: Column(children: [
-          Center(
-            child: Image.asset("assets/logo.png"),
-          ),
-          Padding(
-              padding: EdgeInsets.all(16),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        hintText: "Username",
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Center(
+              child: Image.asset("assets/logo.png"),
+            ),
+            Padding(
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          hintText: "Username",
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Please enter username";
+                          }
+                          return null;
+                        },
                       ),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return "Please enter username";
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        hintText: "Password",
+                      TextFormField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Please enter username";
+                          }
+                          return null;
+                        },
                       ),
-                      obscureText: true,
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return "Please enter username";
-                        }
-                        return null;
-                      },
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        child: Text("Forgot Password?"),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          child: Text("Forgot Password?"),
+                          onPressed: () => {},
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(0),
+                          child: ElevatedButton(
+                              child: Text("Login"),
+                              onPressed: () async {
+                                if (_formKey.currentState.validate())
+                                  {
+                                    dynamic results =
+                                    await context
+                                        .read<AuthenticationService>()
+                                        .signIn(
+                                      email: emailController.text,
+                                      password: passwordController.text,
+                                    );
+
+                                    if(results != null)
+                                      {
+                                        showDialog<void>(
+                                          context: context,
+                                          barrierDismissible: false, // user must tap button!
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title: Text('Error'),
+                                              content: SingleChildScrollView(
+                                                child: ListBody(
+                                                  children: <Widget>[
+
+                                                    Text(results),
+                                                  ],
+                                                ),
+                                              ),
+                                              actions: <Widget>[
+                                                TextButton(
+                                                  child: Text('Done'),
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+                                      }
+                                  }
+                              })),
+                      Padding(padding: EdgeInsets.all(8)),
+                      Text("Not a member yet?"),
+                      TextButton(
+                        child: Text("Register now"),
                         onPressed: () => {},
                       ),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.all(0),
-                        child: ElevatedButton(
-                            child: Text("Login"),
-                            onPressed: () => {
-                                  if (_formKey.currentState.validate())
-                                    {
-                                      context
-                                          .read<AuthenticationService>()
-                                          .signIn(
-                                            email: emailController.text,
-                                            password: passwordController.text,
-                                          )
-                                    }
-                                })),
-                    Padding(padding: EdgeInsets.all(8)),
-                    Text("Not a member yet?"),
-                    TextButton(
-                      child: Text("Register now"),
-                      onPressed: () => {},
-                    ),
-                  ],
-                ),
-              ))
-        ]));
+                    ],
+                  ),
+                ))
+          ]),
+        ),
+
+
+
+        );
   }
 }
